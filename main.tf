b1d4cdcae7fbc03d89d03e30116edc83e752b14b
Arun S <arunlegilsspba@gmail.com>
2025-11-16 01:53:46 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"] # Amazon Linux 2 official owner ID
+}
+
 resource "aws_instance" "app" {
-  ami           = "ami-0522ab6e1a0c5bb0e"
+  ami           = data.aws_ami.amazon_linux_2.id
   instance_type = var.instance_type
 
   tags = {
